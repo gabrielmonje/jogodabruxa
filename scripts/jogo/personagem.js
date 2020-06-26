1bc81094e046d068e8d5925993126701a14e30ee
fabiohide <fheguchi@gmail.com>
2020-06-26 16:57:30 -0300

    fixes

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,32 +1,39 @@
-class Personagem extends Animacao{
-  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite){
+class Personagem extends Animacao {
+  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
     super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
-   
+
     this.yBase = height - this.altura;
     this.y = this.yBase;
     this.velocidadeDoPulo = 0;
     this.gravidade = 3;
   }
-  
+
   pula() {
-    this.velocidadeDoPulo = -30 ;
+    this.velocidadeDoPulo = -30;
   }
 
   aplicaGravidade() {
     this.y = this.y + this.velocidadeDoPulo;
     this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
 
-    if(this.y > this.yBase) {
+    if (this.y > this.yBase) {
       this.y = this.yBase;
     }
 
   }
 
   colidindo(inimigo) {
-      const precisao = .7;
-      const colisao = collideRectRect(this.x, this.y, this.largura * precisao, this.altura * precisao,
-       inimigo.x, inimigo.y, inimigo.largura * precisao, inimigo.altura * precisao);
+    const precisao = .8;
+    const colisao = collideRectCircle(this.x, this.y, this.largura * precisao, this.altura * precisao,
+      inimigo.x + (inimigo.largura / 2) + 5, inimigo.y + (inimigo.altura / 2), inimigo.largura);
 
-      return colisao;
+    //pra ver as hitboxes  
+    /*
+    fill(255, 0, 255, 50);
+    rect(this.x + 6, this.y, this.largura * precisao, 8 + this.altura * precisao, );
+    //rect(inimigo.x, inimigo.y, inimigo.largura * precisao, inimigo.altura * precisao);
+    circle(inimigo.x + (inimigo.largura / 2) + 5, inimigo.y + (inimigo.altura / 2), inimigo.largura);
+    */
+    return colisao;
   }
 }
